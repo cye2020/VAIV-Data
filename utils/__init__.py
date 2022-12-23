756839b82f928c509fbc204d14c54b428b940b2d
Yeeun-Cho <ariaplay101@gmail.com>
2022-12-24 01:41:06 +0900

    util

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .util import *
 from .stock_format import *
+from .mpf import *
--- utils/util.py
+++ utils/util.py
@@ -1,9 +1,26 @@
 import pandas as pd
+from pathlib import Path
+import glob
+import re
+
 
 def dataframe_empty_handler(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except (AttributeError, KeyError, TypeError):
+        except (AttributeError, KeyError, TypeError, FileNotFoundError):
             return pd.DataFrame()
     return wrapper
+
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
