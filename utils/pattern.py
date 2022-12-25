4cd88ba47bd8e247a3a8ed750e0c3cb6923ff09d
Yeeun-Cho <ariaplay101@gmail.com>
2022-12-25 22:15:11 +0900

    Make Labeling

--- utils/pattern.py
+++ utils/pattern.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class CandleStick:
     def __init__(self, open=0, high=0, low=0, close=0) -> None:
         self.open = open
@@ -24,7 +26,7 @@ class Pattern:
             How many candlestick is needed for checking pattern
         '''
         self.num = num
-        self.conditions = []
+        self.conditions = List[Condition]
     
     def __call__(self, date, section) -> bool:
         dates = section.index.tolist()
@@ -46,7 +48,8 @@ class Pattern:
 
 class Bullish:
     def __init__(self) -> None:
-        self.patterns = [self.BullishHarami, self.BullishEngulfing, self.BullishDoji, self.Hammer, self.MoningStar]
+        self.patterns = [self.BullishHarami(), self.BullishEngulfing(), \
+            self.BullishDoji(), self.Hammer(), self.MoningStar()]
 
     def __call__(self, date, section) -> dict:
         result = dict()
@@ -55,46 +58,178 @@ class Bullish:
         return result
     
     class BullishHarami(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
-            
+            self.conditions.append(Condition(bearish_candle, 0))
+            self.conditions.append(Condition(bullish_candle, 1))
+            self.conditions.append(Condition(close_under_open, 0, 1))
+            self.conditions.append(Condition(close_under_open, 1, 0))
+            self.conditions.append(Condition(big_body, 0, 0.6))
+
     class BullishEngulfing(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
-    
+            self.conditions.append(Condition(bearish_candle, 0))
+            self.conditions.append(Condition(bullish_candle, 1))
+            self.conditions.append(Condition(close_above_open, 0, 1))
+            self.conditions.append(Condition(close_above_open, 1, 0))
+            self.conditions.append(Condition(big_body, 1, 0.6))
+
     class BullishDoji(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
+            self.conditions.append(Condition(bearish_candle, 0))
+            self.conditions.append(Condition(lower, 1, 0))
+            self.conditions.append(Condition(gravestone, 3, 1, 1))
+            self.conditions.append(Condition(big_body, 0, 0.6))
 
     class Hammer(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
 
     class MoningStar(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=3) -> None:
             super().__init__(num)
 
 
 class Bearish:
     def __init__(self) -> None:
-        self.patterns = [self.BearishHarami, self.BearishEngulfing, self.GravestoneDoji, self.HangingMan, self.EveningStar]
+        self.patterns = [self.BearishHarami(), self.BearishEngulfing(), \
+            self.GravestoneDoji(), self.HangingMan(), self.EveningStar()]
     
+    def __call__(self, date, section) -> dict:
+        result = dict()
+        for pattern in self.patterns:
+            result[pattern.__class__.__name__] = pattern(date, section)
+        return result
+
     class BearishHarami(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
 
     class BearishEngulfing(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
         
     class GravestoneDoji(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
 
     class HangingMan(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=2) -> None:
             super().__init__(num)
 
     class EveningStar(Pattern):
-        def __init__(self, num) -> None:
+        def __init__(self, num=3) -> None:
             super().__init__(num)
+
+
+class Condition:
+    def __init__(self, func, *args) -> None:
+        self.func = func
+        self.args = args
+    
+    def __call__(self, candlesticks: List[CandleStick]) -> bool:
+        return self.func(candlesticks, *self.args)
+
+
+def bullish_candle(candlesticks: List[CandleStick], n=0):
+    '''
+    n: int
+        index of candlestick to check bullish
+    '''
+    c = candlesticks[n]
+    return c.bullish()
+
+
+def bearish_candle(candlesticks: List[CandleStick], n=0):
+    '''
+    n: int
+        index of candlestick to check bearish
+    '''
+    c = candlesticks[n]
+    return c.bearish()
+
+
+def close_under_open(candlesticks: List[CandleStick], n1=0, n2=1):
+    '''
+    n1: int
+        index of candlestick to compare close with another candlestick's open
+    n2: int
+        index of candlestick to compare open with another candlestick's close
+    '''
+    c1 = candlesticks[n1]
+    c2 = candlesticks[n2]
+    return c1.close < c2.open
+
+
+def close_above_open(candlesticks: List[CandleStick], n1=0, n2=1):
+    '''
+    n1: int
+        index of candlestick to compare close with another candlestick's open
+    n2: int
+        index of candlestick to compare open with another candlestick's close
+    '''
+    c1 = candlesticks[n1]
+    c2 = candlesticks[n2]
+    return c1.close > c2.open
+
+
+def lower(candlesticks: List[CandleStick], n1=0, n2=1):
+    '''
+    n1: int
+        index of candlestick to check if its low is lower than another candlestick's low
+    n2: int
+        index of another candlestick
+    '''
+    c1 = candlesticks[n1]
+    c2 = candlesticks[n2]
+    return c1.low < c2.low
+
+
+def big_body(candlesticks: List[CandleStick], n=0, ratio=0.6):
+    '''
+    n: int
+        index of candlestick to check the ratio of body
+    ratio: float
+        if body account for above 'ratio' of candle, it is big body
+    '''
+    c = candlesticks[n]
+    return c.body > ratio * c.candle
+
+
+def small_body(candlesticks: List[CandleStick], n=0, ratio=0.3):
+    '''
+    n: int
+        index of candlestick to check the ratio of body
+    ratio: float
+        if body account for under 'ratio' of candle, it is big body
+    '''
+    c = candlesticks[n]
+    return c.body < ratio * c.candle
+
+
+def gravestone(candlesticks: List[CandleStick], n=0, ratio1=3, ratio2=1, ratio3=1):
+    '''
+    n: int
+        index of candlestick to check gravestone doji
+    ratio1, ratio2, ratio3: float
+        upper_shadow: body: lower_shadow = ratio1: ratio2: ratio3
+    '''
+    c = candlesticks[n]
+    check1 = c.upper_shadow * ratio2 > c.body * ratio1
+    check2 = c.upper_shadow * ratio3 > c.lower_shadow * ratio1
+    return check1 & check2
+
+
+def dragonfly(candlesticks: List[CandleStick], n=0, ratio1=0.3, ratio2=1, ratio3=2):
+    '''
+    n: int
+        index of candlestick to check dragonfly doji
+    ratio1, ratio2, ratio3: float
+        upper_shadow: body: lower_shadow = ratio1: ratio2: ratio3
+    '''
+    c = candlesticks[n]
+    check1 = c.lower_shadow * ratio2 > c.body * ratio3
+    check2 = c.upper_shadow * ratio2 > c.body * ratio1
+    return check1 & check2
