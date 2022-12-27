4a290223bec272e2711f529861f1d0f0a183b268
cye2020 <ariaplay101@gmail.com>
2022-12-27 12:41:59 +0900

    remove 'ALL' from market option

--- make_stocks.py
+++ make_stocks.py
@@ -4,9 +4,8 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--market', '-m', nargs='+', type=str, dest='market', default='ALL',
+        '--market', '-m', nargs='+', type=str, dest='market', default=['Kospi', 'kosdaq'],
         help='You can input a list of under options\n' + \
-             'ALL: Stock market includes KOSPI, KOSDAQ, and KONEX\n' + \
              'KOSPI: Stock market includes KOSPI only\n' + \
              'KOSDAQ: Stock market includes KOSDAQ only\n' + \
              'KONEX: Stock market includes KONEX only'
