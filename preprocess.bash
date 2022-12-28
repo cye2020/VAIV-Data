bf770464337332691c65196a320c0ec97fde4e41
cye2020 <ariaplay101@gmail.com>
2022-12-28 15:20:52 +0900

    add make candlestick chart

--- preprocess.bash
+++ preprocess.bash
@@ -2,3 +2,11 @@ market=("kospi" "kosdaq")
 
 # make stock historical data
 python make_stocks.py -m ${market[@]}
+
+
+cnn_name="224x224"
+yolo_name="1800x650"
+
+# make candlestick chart
+python make_candlesticks -n ${yolo_name} -m ${market[@]} --yolo
+python make_candlesticks -n ${cnn_name} -m ${market[@]} --cnn
