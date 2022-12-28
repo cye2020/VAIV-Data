e6c0f6c6823e5b23e6f1103096e16d28945b1ccc
cye2020 <ariaplay101@gmail.com>
2022-12-29 00:06:27 +0900

    Upload file

--- merge_labeling.py
+++ merge_labeling.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
-def merge_labeling(data, left_thres, right_thres):
+
+def minmax_drange(data, trade_date, left_thres, right_thres, label):
+    pass
+
+def merge_labeling(data, minmax: pd.DataFrame, patterns: pd.DataFrame, left_thres, right_thres):
     labeling_list = []
     before_drange = {'Label': -1, 'Date': 'empty', 'Range': []}
     
+    for row in minmax.to_dict('records'):
+        label = row.get('Label')
+        trade_date = row.get('Date')
+        prior = row.get('Priority')
+        
+        
